b4e002410c48146f05787a9426a2e7585ebc4268
Aman Verma <amanverma03328@gmail.com>
2022-03-20 00:11:17 +0530

    tool add along with validation done

--- TailWindFrontEnd/static/js/adminPanel.js
+++ TailWindFrontEnd/static/js/adminPanel.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js";
 import { getDatabase, ref, get, set, child, update, remove }
     from "https://www.gstatic.com/firebasejs/9.6.8/firebase-database.js";
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL }
+    from "https://www.gstatic.com/firebasejs/9.6.8/firebase-storage.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,16 +23,25 @@ const databaseRef = ref(db);
 
 
 
+
+
+
+
+
 // event listener
-    // menu
-    document.getElementById("menuButtonProfile").addEventListener("click",controllerDisplay);
-    document.getElementById("menuButtonToolRecommendation").addEventListener("click",controllerDisplay);
-    document.getElementById("menuButtonBlog").addEventListener("click",controllerDisplay);
-    
-    // profile 
-    document.getElementById("reviewImageInp").addEventListener("change",changeProfilePicture);
-    document.getElementById("changePasswordSubmitBtn").addEventListener("click",changeAdminPassword);
-    
+// menu
+document.getElementById("menuButtonProfile").addEventListener("click", controllerDisplay);
+document.getElementById("menuButtonToolRecommendation").addEventListener("click", controllerDisplay);
+document.getElementById("menuButtonBlog").addEventListener("click", controllerDisplay);
+
+// profile 
+document.getElementById("reviewImageInp").addEventListener("change", changeProfilePicture);
+document.getElementById("changePasswordSubmitBtn").addEventListener("click", changeAdminPassword);
+
+// tools
+document.getElementById("addToolSubmitBtn").addEventListener("click", addTool);
+
+
 // event listener end
 
 
@@ -39,15 +50,15 @@ const databaseRef = ref(db);
 
 // functions 
 
-function changeProfilePicture(){
+function changeProfilePicture() {
     alert("hello event fired");
 }
 
-function changeAdminPassword(){
+function changeAdminPassword() {
     let renewPasswordInp = document.getElementById("renewPasswordInp");
     let renewPasswordInpValue = renewPasswordInp.value;
-    
-    if(renewPasswordInpValue.length > 0){
+
+    if (renewPasswordInpValue.length > 0) {
         update(ref(db, "AdminCredential/"), {
             adminPassword: renewPasswordInpValue,
         })
@@ -60,19 +71,19 @@ function changeAdminPassword(){
         });
 
     }
-    else{
+    else {
         alert("empty field");
     }
 
     renewPasswordInp.value = "";
 }
 
-function controllerDisplay(){
+function controllerDisplay() {
     // alert(this.dataset.controllerBoxName);
     let targetedControllerId = this.dataset.controllerBoxName; // because data set contain the exact id of targeted element
     let controllerContainerArray = document.getElementsByClassName("controllerContainer");
 
-    for( let containerItr of controllerContainerArray){
+    for (let containerItr of controllerContainerArray) {
         // console.log(containerItr);
         containerItr.classList.remove("block");
         containerItr.classList.add("hidden");
@@ -82,6 +93,69 @@ function controllerDisplay(){
     document.getElementById(targetedControllerId).classList.remove("hidden");
     document.getElementById(targetedControllerId).classList.add("block");
 }
+
+function addTool() {
+    let addToolNameInp = document.getElementById("addToolNameInp");
+    let addToolUrlInp = document.getElementById("addToolUrlInp");
+    let addToolDescriptionInp = document.getElementById("addToolDescriptionInp");
+
+    
+
+    if( addToolNameInp.value === "" ){
+        alert("tool name field cannot be empty");
+    }else if( addToolUrlInp.value === ""){
+        alert("tool url field cannot be empty");
+    }else if( addToolDescriptionInp.value === ""){
+        alert("tool description field cannot be empty");        
+    }
+    else{
+        get(child(databaseRef, "SerialCount/toolSerialCount"))
+        .then((snapshot) => { 
+            let currentSerialNumber = snapshot.val();
+            let newToolId = currentSerialNumber + 1
+            // console.log(currentSerialNumber+ "is the current serial count");
+
+            set(ref(db, "Tools/" + newToolId), {
+                toolId: newToolId,
+                toolName: addToolNameInp.value.trim(),
+                toolUrl: addToolUrlInp.value,
+                toolDescription: addToolDescriptionInp.value,
+            })
+            .then(() => {
+                console.log("tool added successfully");
+                updateToolSerialCount(currentSerialNumber+1);
+
+                // emptying the fields
+                addToolNameInp.value = "";
+                addToolUrlInp.value = "";
+                addToolDescriptionInp.value = "";
+            })
+            .catch((error) => {
+                console.log(error);
+                console.log("error aa gai h tool entry m");
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log("error aa gai h toolSerialCount fetch krne m");
+        });
+    }
+}
+
+
+function updateToolSerialCount( updatedCount ){
+    update(ref(db, "SerialCount"), {
+        toolSerialCount: updatedCount,
+    })
+    .then(() => {
+        console.log("tool serialCount updated successfully");
+    })
+    .catch((error) => {
+        console.log(error);
+        console.log("error aa gai h tool serial count update m");
+    });    
+}
+
 
 
 // functions end
